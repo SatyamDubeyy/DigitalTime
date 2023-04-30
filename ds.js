c14c774c6eeab3055ac8a2b77eaa1279556e3329
SatyamDubeyy <98874638+SatyamDubeyy@users.noreply.github.com>
2023-04-30 12:52:26 +0530

    Initial Commit

---
+++ ds.js
@@ -0,0 +1,16 @@
+let id=(document.getElementsByTagName("p"))['0']
+
+
+const tset=()=>{
+    let d=new Date();
+id.innerText=d;
+}
+
+console.log(id.innerText);
+setInterval(tset,1000)
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+  id.innerHTML = "Created By Satyam Dubey";
+}
